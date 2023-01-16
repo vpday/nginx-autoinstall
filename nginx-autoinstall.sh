6a7b458ad7fb8f6b951550e4205b1187729a538d
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2023-01-16 15:27:51 -0500

    Update Brotli to Master

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -282,7 +282,6 @@ case $OPTION in
 		cd /usr/local/src/nginx/modules || exit 1
 		git clone https://github.com/google/ngx_brotli
 		cd ngx_brotli || exit 1
-		git checkout v1.0.0rc
 		git submodule update --init
 	fi
 
