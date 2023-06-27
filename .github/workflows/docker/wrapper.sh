1cbc9060fe37e4f1d3166abc7c05b6587d0e8266
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2023-06-27 18:48:20 -0400

    Update wrapper.sh

--- .github/workflows/docker/wrapper.sh
+++ .github/workflows/docker/wrapper.sh
@@ -3,23 +3,23 @@
 export HEADLESS=y
 
 if [[ $INSTALL_TYPE == "FULL" ]]; then
-    #export PAGESPEED=y
+    export PAGESPEED=n # Support Retired
     export BROTLI=y
     export HEADERMOD=y
     export GEOIP=n # requires license key
     export FANCYINDEX=y
     export CACHEPURGE=y
     export SUBFILTER=y
-    #export LUA=y
+    #export LUA=y # Module faulty, should be tested with the most recent release of the module to ensure proper operation.
     export WEBDAV=y
     export VTS=y
     export RTMP=y
     export TESTCOOKIE=y
-    export HTTP3=y
+    export HTTP3=n Not Working Properly With Recent & now unnecessary with mainline http3
     export MODSEC=y
     export HPACK=y
     export REDIS2=y
-    #export HTTPREDIS=y
+    export HTTPREDIS=n # Not Working Properly
     export SRCACHE=y
     export SETMISC=y
     export NGXECHO=y
