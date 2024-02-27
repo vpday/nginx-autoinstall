141041cc735580c90dcb9f6872d0198314f40e0a
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2024-02-27 09:05:17 -0500

    Shell Lint

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -281,10 +281,10 @@ case $OPTION in
 	if [[ $BROTLI == 'y' ]]; then
 	    cd /usr/local/src/nginx/modules || exit 1
 		git clone https://github.com/google/ngx_brotli
-		cd ngx_brotli/deps/brotli
-        mkdir out && cd out
-        cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
-        cmake --build . --config Release --target brotlienc
+		cd ngx_brotli/deps/brotli || exit
+    mkdir out && cd out || exit
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
+    cmake --build . --config Release --target brotlienc
 	fi
 
 	# More Headers
